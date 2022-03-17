a4dc9899d9eacfe332af8d381f65076f04b76a64
Alex Paredes <alexparedesr95@gmail.com>
2022-03-17 17:57:38 -0600

    FEAT: Added elements and navigation functionability to the LoginScreen component.

--- src/components/login/LoginScreen.js
+++ src/components/login/LoginScreen.js
@@ -1,8 +1,27 @@
+import { useNavigate } from "react-router-dom";
+
 export const LoginScreen = () => {
-    return (
-      <div>
-          <h1>LoginScreen</h1>
-      </div>
-    )
+
+	const navigate = useNavigate();
+
+	const handleLogin = () => {
+		navigate( '/', {
+			replace: true,
+		});
+	};
+
+      return (
+        <div className="container mt-5">
+            <h1>Login</h1>
+            <hr />
+            
+            <button
+				className="btn btn-primary"
+				onClick={ handleLogin }
+            >
+              	Login
+            </button>
+        </div>
+      )
   }
   

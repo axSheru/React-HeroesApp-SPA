23348ef3945c020f1692b41dd4594d03c7b1367c
Alex Paredes <alexparedesr95@gmail.com>
2022-03-18 13:54:57 -0600

    FEAT: Used AuthContext to get user data.

--- src/ui/Navbar.js
+++ src/ui/Navbar.js
@@ -1,7 +1,10 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import { Link, NavLink, useNavigate } from 'react-router-dom'
+import { AuthContext } from '../auth/authContext';
 
 export const Navbar = () => {
+
+    const { user } = useContext( AuthContext );
 
     const navigate = useNavigate();
 
@@ -51,7 +54,7 @@ export const Navbar = () => {
                 <ul className="navbar-nav ml-auto">
 
                     <span className='nav-item nav-link text-info'>
-                        Alex
+                        { user.name }
                     </span>
 
                     <button 

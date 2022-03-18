9f3b5a9358a0eb1626e0c7be28034e05d44a4b80
Alex Paredes <alexparedesr95@gmail.com>
2022-03-18 15:17:37 -0600

    FEAT: Added PublicRoute as a high order component to handle access to public routes.

--- src/routers/AppRouter.js
+++ src/routers/AppRouter.js
@@ -2,12 +2,17 @@ import { Route, Routes, BrowserRouter } from 'react-router-dom';
 import { LoginScreen } from '../components/login/LoginScreen';
 import { DashboardRoutes } from './DashboardRoutes';
 import { PrivateRoute } from './PrivateRoute';
+import { PublicRoute } from './PublicRoute';
 
 export const AppRouter = () => {
     return (
         <BrowserRouter>
             <Routes>
-                <Route path="/login" element={<LoginScreen />} />
+                <Route path="/login" element={
+                    <PublicRoute>
+                        <LoginScreen />
+                    </PublicRoute>
+                } />
 
                 <Route path="/*" element={
                     <PrivateRoute>

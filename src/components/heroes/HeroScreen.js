8cf92b2ee679ba8c048dcdbae416b65169c5f515
Alex Paredes <alexparedesr95@gmail.com>
2022-03-17 20:58:49 -0600

    FEAT: Impemented useMemo hook.

--- src/components/heroes/HeroScreen.js
+++ src/components/heroes/HeroScreen.js
@@ -1,3 +1,4 @@
+import { useMemo } from "react";
 import { Navigate, useNavigate, useParams } from "react-router-dom";
 import { getHeroById } from "../../selectors/getHeroById";
 
@@ -6,7 +7,7 @@ export const HeroScreen = () => {
 	const { heroId } = useParams();
 	const navigate = useNavigate();
 
-	const hero = getHeroById( heroId );
+	const hero = useMemo( () => getHeroById( heroId ), [ heroId ] );
 
 	const handleReturn = () => {
 		navigate( -1 );

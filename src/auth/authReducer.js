f3ee7876ea7346acaa4365dc1b705050e0da6983
Alex Paredes <alexparedesr95@gmail.com>
2022-03-18 11:46:01 -0600

    FEAT: Created authReducer to handle the actions dispatch.

---
+++ src/auth/authReducer.js
@@ -0,0 +1,26 @@
+import { types } from "../types/types";
+
+/* const state = {
+    name: 'Alex',
+    logged: true,
+}; */
+
+export const authReducer = ( state = {}, action ) => {
+
+    switch ( action.type ) {
+        case types.login:
+            return {
+                ...action.payload,// name: action.payload.name
+                logged: true,
+            };
+
+        case types.logout:
+            return {
+                logged: false,
+            };
+    
+        default:
+            return state;
+    }
+
+};

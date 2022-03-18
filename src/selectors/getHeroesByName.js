18754004b07a760fd2ff4d5a2ac854378c1e5c3e
Alex Paredes <alexparedesr95@gmail.com>
2022-03-17 21:35:53 -0600

    FEAT: Created selector to filter heroes by name.

---
+++ src/selectors/getHeroesByName.js
@@ -0,0 +1,6 @@
+import { heroes } from "../data/heroes";
+
+
+export const getHeroesByName = ( name = '' ) => {
+    return heroes.filter( hero => hero.superhero.toLowerCase().includes( name ) );
+};

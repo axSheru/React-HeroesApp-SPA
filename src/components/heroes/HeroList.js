695124430205d39b1f3ce1769f5c60d644e0fdbd
Alex Paredes <alexparedesr95@gmail.com>
2022-03-17 19:12:37 -0600

    FEAT: Created component to list the given heroes.

---
+++ src/components/heroes/HeroList.js
@@ -0,0 +1,26 @@
+import PropTypes from 'prop-types';
+import { getHeroesByPublisher } from "../../selectors/getHeroesByPublisher";
+
+
+export const HeroList = ({ publisher }) => {
+
+    const heroes = getHeroesByPublisher( publisher );
+
+    return (
+        <>
+            <ul>
+                {
+                    heroes.map( hero => (
+                        <li key={ hero.id }>
+                            { hero.superhero }
+                        </li>
+                    ))
+                }
+            </ul>
+        </>
+    );
+};
+
+HeroList.propTypes = {
+    publisher: PropTypes.string.isRequired.oneOf(['Marvel Comics', 'DC Comics']),
+};

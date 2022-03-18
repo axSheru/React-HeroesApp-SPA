05a841fd7560f51dbc47b3905f2bc311874e6504
Alex Paredes <alexparedesr95@gmail.com>
2022-03-18 14:31:54 -0600

    FEAT: Added ability to save in the context the user data when it logs in.

--- src/components/login/LoginScreen.js
+++ src/components/login/LoginScreen.js
@@ -1,10 +1,25 @@
+import { useContext, useReducer } from "react";
 import { useNavigate } from "react-router-dom";
+import { AuthContext } from "../../auth/authContext";
+import { types } from "../../types/types";
 
 export const LoginScreen = () => {
+
+  const { dispatch } = useContext( AuthContext );
 
 	const navigate = useNavigate();
 
 	const handleLogin = () => {
+
+    const action = {
+      type: types.login,
+      payload: {
+        name: 'Alex',
+      },
+    };
+
+    dispatch( action );
+
 		navigate( '/', {
 			replace: true,
 		});

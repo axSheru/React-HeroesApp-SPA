03406a5b109437ffe926829a833cf2cccabae4fe
Alex Paredes <alexparedesr95@gmail.com>
2022-03-17 21:27:00 -0600

    FEAT: Added structure and search input functionality to SearchScreen component.

--- src/components/search/SearchScreen.js
+++ src/components/search/SearchScreen.js
@@ -1,8 +1,47 @@
+import { useForm } from "../../hooks/useForm";
+
 export const SearchScreen = () => {
+
+	const [ formValues, handleInputChange, reset ] = useForm({
+		searchText: '',
+	});
+
+	const { searchText } = formValues;
+
+	const handleSearch = ( e ) => {
+		e.preventDefault();
+		console.log( searchText )
+	};
+
     return (
-      <div>
-          <h1>SearchScreen</h1>
-      </div>
+		<>
+			<h1>Búsquedas</h1>
+			<hr />
+
+			<div className="row">
+				<div className="col-5">
+					<h4>Buscar</h4>
+					<hr />
+					<form onSubmit={ handleSearch }>
+						<input
+							type="text"
+							placeholder="Busca un héroe..."
+							className="form-control"
+							name="searchText"
+							autoComplete="off"
+							value={ searchText }
+							onChange={ handleInputChange }
+						/>
+						<button
+							type="submit"
+							className="btn btn-outline-primary mt-1 btn-block"
+						>
+							Buscar...
+						</button>
+					</form>
+				</div>
+			</div>
+		</>
     )
   }
   

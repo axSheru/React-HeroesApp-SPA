3f06b4c40d60a8d04168831ff08f5b386c555d3c
Alex Paredes <alexparedesr95@gmail.com>
2022-03-18 20:11:31 -0600

    TEST: Added tests for authReducer.

---
+++ src/tests/auth/authReducer.spec.js
@@ -0,0 +1,41 @@
+import { authReducer } from "../../auth/authReducer";
+import { types } from "../../types/types";
+
+describe('Pruebas en el authReducer.', () => {
+
+    test('debe de retornar el estado por defecto.', () => {
+
+        const state = authReducer( { logged: false }, {} );
+
+        expect( state ).toEqual({ logged: false });
+
+    });
+
+    test('debe de autenticar y colocar el "name" del usuario y poner "logged" en true.', () => {
+
+        const action = {
+            type: types.login,
+            payload: {
+                name: 'Alex',
+            },
+        };
+
+        const state = authReducer( { logged: false }, action );
+
+        expect( state ).toEqual({ logged: true, name: 'Alex' });
+
+    });
+
+    test('debe de borrar el "name" del usuario y poner "logged" en false.', () => {
+
+        const action = {
+            type: types.logout,
+        };
+
+        const state = authReducer( { logged: true, name: 'Alex' }, action );
+
+        expect( state ).toEqual({ logged: false });
+
+    });
+    
+});
